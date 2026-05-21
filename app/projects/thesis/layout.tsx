dcc5bb9bb4b009907d437f99b758cee538312170
JACOB PRATT <jacobpratt@JACOBs-MacBook-Pro.local>
2026-05-21 18:52:18 -0500

    initial commmit for next.js app.

---
+++ app/projects/thesis/layout.tsx
@@ -0,0 +1,24 @@
+
+export default function ThesisLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+      return (
+          <main className="flex flex-col">
+              {children}
+          </main>
+  );
+}
+
+//<nav className="sticky top-0 w-full bg-inherit border-b shadow-sm z-50">
+//    <div className="max-w-7xl mx-auto px-4 h-16 flex items-center justify-center">
+//        <div className="flex space-x-8 font-medium">
+//            <a href="/" className="hover:text-gray-500 transition">HOME</a>
+//            <a href="/projects" className="hover:text-gray-500 transition">PROJECTS</a>
+//        </div>
+//    </div>
+//</nav>
+
+//<a href="#" className="hover:text-blue-600 transition">Pricing</a>aria-current="page" className="rounded-md
+//<a href="#" className="hover:text-blue-600 transition">About</a>
