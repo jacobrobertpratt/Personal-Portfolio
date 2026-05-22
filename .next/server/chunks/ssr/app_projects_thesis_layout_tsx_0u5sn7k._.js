724b2c1defb8514bf55303c5bbd044dbc71ee76a
JACOB PRATT <isuandme@gmail.com>
2026-05-21 21:25:15 -0500

    idk what happened. need to add '.next' directory to .gitignore

--- .next/server/chunks/ssr/app_projects_thesis_layout_tsx_0u5sn7k._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[10623,a=>{"use strict";var b=a.i(7997);a.s(["default",0,function({children:a}){return(0,b.jsx)("main",{className:"flex flex-col",children:a})}])},49506,a=>{a.n(a.i(10623))}];
-
-//# sourceMappingURL=app_projects_thesis_layout_tsx_0u5sn7k._.js.map
